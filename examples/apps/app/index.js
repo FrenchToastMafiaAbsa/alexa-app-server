7581ae4c57c11819e37aa815860685b64e4f87f1
Alexandre Scheurkogel <alex.scheurkogel@dariel.co.za>
2017-09-20 17:16:47 +0200

    balance POC intent

--- examples/apps/app/index.js
+++ examples/apps/app/index.js
@@ -3,6 +3,22 @@ var alexa = require('alexa-app');
 // Allow this module to be reloaded by hotswap when changed
 module.change_code = 1;
 
+
+var startConf={
+  chequeAccount:{
+    balance:103000,
+    transcations:[
+      5000,1000,2000
+    ]
+  },
+  creditAccount:{
+    balance:-90000
+  }
+}
+
+var accountScope = null;
+
+
 // Define an alexa-app
 var app = new alexa.app('app');
 app.id = require('./package.json').alexa.applicationId;
@@ -15,7 +31,7 @@ app.intent('balanceIntent', {
   "slots": {},
   "utterances": []
 }, function(req, res) {
-  res.say('Fuck you divar');
+  res.say('Your bank balance is '+ startConf.chequeAccount.balance+" rand");
 });
 
 module.exports = app;
